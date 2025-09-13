8c76236daab46307105408a31950242c5b9fdc0f
Akxin Laxinbault <kheijhackt3@gmail.com>
2025-09-13 21:00:45 +0800

    Applied accordion to make forms inside a dropdown

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Forms
+document.addEventListener('DOMContentLoaded', function () {
+  const accordions = document.querySelectorAll('.accordion-collapse');
+
+  accordions.forEach(acc => {
+    acc.addEventListener('show.bs.collapse', function () {
+      const iframe = this.querySelector('iframe');
+      if (iframe) iframe.style.height = "1300px";
+    });
+
+    acc.addEventListener('hide.bs.collapse', function () {
+      const iframe = this.querySelector('iframe');
+      if (iframe) iframe.style.height = "0px";
+    });
+  });
+});
